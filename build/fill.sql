03d07c065ea79d9d665fe63f9560d0138278a857
zato4en <lavrenovmihail2103@gmail.com>
2023-12-20 20:25:16 +0300

    lab5 pravki done and showed

--- build/fill.sql
+++ build/fill.sql
@@ -1,5 +1,5 @@
 --
-INSERT INTO users (user_name, login, password, role) VALUES ('aboba','user1', 'user1', 0);
+-- INSERT INTO users (user_name, login, password, role) VALUES ('aboba','user1', 'user1', 0);
 -- INSERT INTO users (login, password, is_admin) VALUES ('user4', 'admin', true);
 -- INSERT INTO users (login, password, is_admin) VALUES ('user2', 'user2', false);
 -- INSERT INTO users (login, password, is_admin) VALUES ('user3', 'user3',false);
@@ -13,10 +13,10 @@ INSERT INTO spectrums (name, len , freq, Description,  Image  ,is_delete) VALUES
 INSERT INTO spectrums (name, len , freq, Description,  Image  ,is_delete) VALUES ('CMB3', 7.35, 40.86,'CMB3 - имеет длину волны 7.35мм и частоту 40.86 Ггц', 'http://127.0.0.1:9000/spectrumbucket/CMB3.jpeg',false);
 
 
---
-INSERT INTO satellites (id, date_created, date_formed, date_accepted, status, percentage, satellite, user_id, moder_id, user_login) VALUES (1,'2010-01-01','2011-01-01', '2022-01-10', 'создан', '0%' ,'Planck', 1,2, 'user1');
-INSERT INTO satellites (id, date_created, date_formed, date_accepted, status, percentage, satellite, user_id, moder_id, user_login) VALUES (2,'2010-01-01','2011-01-01', '2022-01-10', 'создан', '0%' ,'mem', 1,2, 'user1');
-INSERT INTO satellites (id, date_created, date_formed, date_accepted, status, percentage, satellite, user_id, moder_id, user_login) VALUES (3,'2010-01-01','2011-01-01', '2022-01-10', 'создан', '0%' ,'test', 1,2, 'user1');
+-- --
+-- INSERT INTO satellites (id, date_created, date_formed, date_accepted, status, percentage, satellite, user_id, moder_id, user_login) VALUES (1,'2010-01-01','2011-01-01', '2022-01-10', 'создан', '0%' ,'Planck', 1,2, 'user1');
+-- INSERT INTO satellites (id, date_created, date_formed, date_accepted, status, percentage, satellite, user_id, moder_id, user_login) VALUES (2,'2010-01-01','2011-01-01', '2022-01-10', 'создан', '0%' ,'mem', 1,2, 'user1');
+-- INSERT INTO satellites (id, date_created, date_formed, date_accepted, status, percentage, satellite, user_id, moder_id, user_login) VALUES (3,'2010-01-01','2011-01-01', '2022-01-10', 'создан', '0%' ,'test', 1,2, 'user1');
 
 
 --
